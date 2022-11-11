c5ce9fad16e69c2c47c07c22ac40c252b41542b5
ken <metal852794613@gmail.com>
2022-11-11 13:24:10 +0800

    fix-NavBar & update .gitignore

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <template v-if="this.$store.state.isAuthenticated && this.$store.state.currentUser.isAdmin === 1">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">管理者後台</a>
+        <a class="navbar-brand">管理者後台</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
